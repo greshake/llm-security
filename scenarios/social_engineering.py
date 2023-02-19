e7a05c629bdc646e10b1427c32a879ee8d6a90c9
Kai Greshake <development@kai-greshake.de>
2023-02-19 21:37:18 +0100

    descriptions

---
+++ scenarios/social_engineering.py
@@ -0,0 +1,8 @@
+"""
+This example demonstrates how an infected language model
+can psychologically manipulate the user into performing
+actions that are not in their best interest or divulging
+sensitive information to an attacker. It can deploy a
+variety of social engineering techniques in an automated
+fashion.
+"""
---
+++ scenarios/surveillance.py
@@ -0,0 +1,6 @@
+"""
+This example demonstrates how an ingestion-only pipeline that might be
+deployed by nation-states for flagging suspicious activity can be exploited
+in a black-box attack if the attacker can trigger a side channel to
+exfiltrate data.
+"""
