2d145c100bd6517d9afad906577113f73d56b9dc
Kai Greshake <development@kai-greshake.de>
2023-02-22 09:55:03 +0100

    update

--- scenarios/data_analysis.py
+++
@@ -1,6 +0,0 @@
-"""
-This example demonstrates how an ingestion-only pipeline that might be
-deployed by nation-states for flagging suspicious activity can be exploited
-in a black-box attack if the attacker can trigger a side channel to
-exfiltrate data.
-"""
